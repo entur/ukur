3de3338dfb029db45b7b9109aad1053ed540fbe3
Lasse Tyrihjell <lassetyr@gmail.com>
2019-02-18 10:10:38 +0100

    Increase loglevel for push-response

--- src/main/java/org/entur/ukur/subscription/SubscriptionManager.java
+++ src/main/java/org/entur/ukur/subscription/SubscriptionManager.java
@@ -587,7 +587,7 @@ public class SubscriptionManager {
             out.flush();
             out.close();
             int responseCode = connection.getResponseCode();
-            logger.trace("Receive {} on push to {} for subscription with id {}", responseCode, subscription.getPushAddress(), subscription.getId());
+            logger.info("Receive {} on push to {} for subscription {}", responseCode, pushAddress, subscription);
             return HttpStatus.valueOf(responseCode);
         } catch (Exception e) {
             logger.warn("Could not push to {} for subscription with id {}", subscription.getPushAddress(), subscription.getId(), e);
